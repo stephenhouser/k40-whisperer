e63081f694250d9a71bd1b8394ae92f6d4c1ff6d
Stephen Houser <stephenhouser@gmail.com>
2019-09-08 13:03:40 -0400

    v0.36-pyinstaller

--- py2app_setup.py
+++ py2app_setup.py
@@ -11,7 +11,6 @@ from setuptools import setup
 
 app_name = 'K40 Whisperer'
 app_version = "0.36"
-
 app_copyright = u'Copyright © 2017-2019, Scorch Works, GNU General Public License'
 main_script = 'k40_whisperer.py'
 url = 'https://github.com/stephenhouser/k40-whisperer'
